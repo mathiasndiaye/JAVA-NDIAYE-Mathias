c56f4d51dfaa4609f00be063c755bf11fd475f8e
Mathias <mathiasndiaye19350@gmail.com>
2022-04-19 15:22:51 +0200

    Fourth version

--- Marketplace/src/marketplace/Marketplace.java
+++ Marketplace/src/marketplace/Marketplace.java
@@ -1,7 +1,8 @@
 package marketplace;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 
@@ -9,8 +10,21 @@ public class Marketplace {
 	
 
 	// Variables de la classe Marketplace
-	private ArrayList<Produit> listeProduits = new ArrayList<Produit>();
-	private Set<User> listeUsers= new HashSet<User>();
+	private ArrayList<Produit> listeProduits;
+	private ArrayList<User> listeUsers;
+	
+	//Indexation des User
+	private Map<String,User> indexLoginUser;     //Permet de mapper un login avec son User
+	private Map<Integer,Produit> indexRefProduit;   //Permet de mapper une ref produit avec son produit
+	
+	
+	//Constructeur de la classe Marketplace
+	public Marketplace() {
+		listeProduits=new ArrayList<Produit>();
+		listeUsers=new ArrayList<User>();
+		indexLoginUser= new HashMap<String,User>();
+		indexRefProduit=new HashMap<Integer,Produit>();
+	}
 	
 	
 	// Getters et Setters
@@ -18,10 +32,9 @@ public class Marketplace {
 		return listeProduits;
 	}
 	
-	public Set<User> getListeUsers() {
+	public ArrayList<User> getListeUsers() {
 		return listeUsers;
 	}	
-	
 	
 	
 	// Méthodes de la classe Marketplace
@@ -31,14 +44,48 @@ public class Marketplace {
 	}
 	
 	public boolean addProduit(Produit p) {
-		return this.listeProduits.add(p);
+		boolean ajoutOK;
+		
+		//Verifie si notre indexation par login ne contient pas déjà le User 
+		if(indexRefProduit.containsKey(p.getReference())) {
+			//on refuse l'ajout du User
+			return false;
+			}
+		
+		//Ajout du User dans la liste de user
+		ajoutOK= listeProduits.add(p);
+				
+		//Indexation du User
+		if(ajoutOK) {
+			indexRefProduit.put(p.getReference(), p);
+		}
+				
+		return ajoutOK;		
 	}
-
+	
 	public boolean addUser(User u) {
-		return this.listeUsers.add(u);
+		boolean ajoutOK;
+		
+		//Verifie si notre indexation par login ne contient pas déjà le User 
+		if(indexLoginUser.containsKey(u.getLogin())) {
+			//on refuse l'ajout du User
+			return false;
+		}
+		
+		//Ajout du User dans la liste de user
+		ajoutOK= listeUsers.add(u);
+		
+		//Indexation du User
+		if(ajoutOK) {
+			indexLoginUser.put(u.getLogin(), u);
+		}
+		
+		return ajoutOK;
 	}
 	
 	public boolean removeUser(User u) {
 		return this.listeUsers.remove(u);
 	}
+	
+	//public boolean removeUserLogin()
 }
